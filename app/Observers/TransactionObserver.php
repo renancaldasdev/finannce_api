4f36c51fdbc082053f06e147d2723ca73711ca20
Renan Caldas <renan@renancaldas.dev.br>
2026-08-08 18:29:55 -0300

    [main] feat: Criando observer para regra de negocio

---
+++ app/Observers/TransactionObserver.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Observers;
+
+use App\Models\Transaction;
+
+class TransactionObserver
+{
+    /**
+     * Handle the Transaction "created" event.
+     */
+    public function created(Transaction $transaction): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Transaction "updated" event.
+     */
+    public function updated(Transaction $transaction): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Transaction "deleted" event.
+     */
+    public function deleted(Transaction $transaction): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Transaction "restored" event.
+     */
+    public function restored(Transaction $transaction): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Transaction "force deleted" event.
+     */
+    public function forceDeleted(Transaction $transaction): void
+    {
+        //
+    }
+}
